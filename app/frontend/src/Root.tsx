6a481687b04cd13dfd20a15dde068b05bfa83d59
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-07-30 17:56:20 +0100

    fixed prettier

--- app/frontend/src/Root.tsx
+++ app/frontend/src/Root.tsx
@@ -1,10 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { BrowserRouter } from "react-router";
 import App from "@/App";
+
+const queryClient = new QueryClient()
 
 export default function Root(): React.ReactNode {
   return (
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   )
 }
--- app/frontend/src/lib/fetch.ts
+++
@@ -1,6 +0,0 @@
-interface APIResponse<T,K> {
-  status: "success" | "failure",
-  payload: null | T,
-  errors: null | K,
-  message: string
-}
