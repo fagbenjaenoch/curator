e77c18b6696c8f22b67ab03e819b34ce41d52321
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-08-06 14:24:09 +0100

    fixed multiple routes showing at once

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,16 +1,19 @@
-import { Routes, Route } from "react-router"
-import Home from "@/components/pages/Home"
-import Landing from "@/components/pages/Landing"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "@/components/pages/Home";
+import Landing from "@/components/pages/Landing";
 
-function App() {
+const queryClient = new QueryClient();
+
+export default function Root(): React.ReactNode {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/app" element={<Home />} />
-      </Routes>
-    </div>
-  )
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/app" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
-
-export default App
--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import "./index.css"
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Root from "./Root";
+import App from "@/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Root />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
