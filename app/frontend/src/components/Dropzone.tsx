00862c8e0ef5a65475b05af1c6fdaef01523d3e3
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-08-07 22:54:07 +0100

    added file size limit to dropzone

--- app/frontend/src/components/Dropzone.tsx
+++ app/frontend/src/components/Dropzone.tsx
@@ -14,6 +14,7 @@ export default function Dropzone(props: React.HTMLAttributes<HTMLDivElement>) {
         [".docx"],
       "text/markdown": [".md"],
     },
+    maxSize: 20 * 1024 * 1024, // 20MB
   });
 
   return (
