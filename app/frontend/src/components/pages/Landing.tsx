61c054f98a99780b7d987504967f55707811e2e5
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-08-12 13:54:42 +0100

    refactored footer

--- app/frontend/src/components/pages/Landing.tsx
+++ app/frontend/src/components/pages/Landing.tsx
@@ -1,10 +1,6 @@
 import Navbar from "@/components/Navbar";
 import ovalMarker from "@/assets/oval marker.svg";
 import Dropzone from "@/components/Dropzone";
-import Github from "@/components/ui/Github";
-import X from "@/components/ui/X";
-import StatusIndicator from "@/components/StatusIndicator";
-
 export default function Landing() {
   return (
     <div>
@@ -26,33 +22,6 @@ export default function Landing() {
           <Dropzone />
         </div>
       </section>
-      <footer>
-        <div className="max-w-7xl mx-auto px-8 py-8">
-          <div className="flex justify-between">
-            <div className="space-y-2">
-              <small className="block">Built with a step out of comfort</small>
-              <p className="text-xs text-green-500 bg-green-100 border-2 border-green-500 w-max p-1 px-2 rounded-full inline-flex items-center gap-1">
-                <StatusIndicator />
-                <span>Available for hire</span>
-              </p>
-            </div>
-            <div className="space-x-4 text-xs">
-              <a
-                href="http://github.com/fagbenjaenoch"
-                className="hover:text-muted-foreground"
-              >
-                <Github className="inline-block hover:text-muted-foreground" />
-              </a>
-              <a
-                href="http://x.com/fagbenjaenoch"
-                className="hover:text-muted-foreground"
-              >
-                <X className="inline-block" />
-              </a>
-            </div>
-          </div>
-        </div>
-      </footer>
     </div>
   );
 }
