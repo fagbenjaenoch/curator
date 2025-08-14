ab8be74c5b965a3b5bf06900a58b29f423bca605
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-08-14 03:07:57 +0100

    created parser service

--- app/backend/internal/routes/route.go
+++ app/backend/internal/routes/route.go
@@ -1,20 +1,16 @@
 package routes
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
 
-	"github.com/fagbenjaenoch/curator/app/backend/internal/config"
+	"github.com/fagbenjaenoch/curator/app/backend/internal/services"
 	"github.com/go-chi/chi"
-	"github.com/rs/zerolog"
-	"rsc.io/pdf"
 )
 
 const MAX_UPLOAD_SIZE int64 = 20 * 1024 * 1024
 
-var logger zerolog.Logger = config.Logger()
+// var logger zerolog.Logger = config.Logger()
 
 func RegisterApiRoues() chi.Router {
 	r := chi.NewRouter()
@@ -37,43 +33,45 @@ func RegisterApiRoues() chi.Router {
 
 		files := r.MultipartForm.File["files"]
 
-		for _, fileHeader := range files {
-			file, err := fileHeader.Open()
-			if err != nil {
-				http.Error(w, "could not open file", http.StatusBadRequest)
-				return
-			}
-			defer file.Close()
+		uploadService := services.NewParser()
+		uploadService.ParsePDF(files)
+		// for _, fileHeader := range files {
+		// 	file, err := fileHeader.Open()
+		// 	if err != nil {
+		// 		http.Error(w, "could not open file", http.StatusBadRequest)
+		// 		return
+		// 	}
+		// 	defer file.Close()
 
-			buff := make([]byte, 512)
-			_, err = file.Read(buff)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			}
+		// 	buff := make([]byte, 512)
+		// 	_, err = file.Read(buff)
+		// 	if err != nil {
+		// 		http.Error(w, err.Error(), http.StatusBadRequest)
+		// 		return
+		// 	}
 
-			r, err := pdf.NewReader(bytes.NewReader(buff), int64(len(buff)))
-			if err != nil {
-				logger.Fatal().Msg(fmt.Sprintf("failed to read pdf: %v", err))
-			}
+		// 	r, err := pdf.NewReader(bytes.NewReader(buff), int64(len(buff)))
+		// 	if err != nil {
+		// 		logger.Fatal().Msg(fmt.Sprintf("failed to read pdf: %v", err))
+		// 	}
 
-			// Loop through pages and print text
-			numPages := r.NumPage()
-			for i := 1; i <= numPages; i++ {
-				page := r.Page(i)
-				if page.V.IsNull() {
-					continue
-				}
+		// 	// Loop through pages and print text
+		// 	numPages := r.NumPage()
+		// 	for i := 1; i <= numPages; i++ {
+		// 		page := r.Page(i)
+		// 		if page.V.IsNull() {
+		// 			continue
+		// 		}
 
-				content := page.Content()
-				text := ""
-				for _, t := range content.Text {
-					text += t.S
-				}
+		// 		content := page.Content()
+		// 		text := ""
+		// 		for _, t := range content.Text {
+		// 			text += t.S
+		// 		}
 
-				fmt.Printf("Page %d:\n%s\n", i, text)
-			}
-		}
+		// 		fmt.Printf("Page %d:\n%s\n", i, text)
+		// 	}
+		// }
 
 		json.NewEncoder(w).Encode(struct {
 			Message string `json:"message"`
