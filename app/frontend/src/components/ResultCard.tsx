cbab2eb91fbfed60e0107df9456912d0eacb8cb6
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-08-29 20:03:02 +0100

    improved result card ui

--- app/frontend/src/components/ResultCard.tsx
+++ app/frontend/src/components/ResultCard.tsx
@@ -6,7 +6,7 @@ interface ResultCardProps {
 
 export default function ResultCard({ title, thumbUrl, url }: ResultCardProps) {
   return (
-    <div className="p-4 shadow-md transition-transform ease-in-out duration-300 hover:translate-y-[-5px]">
+    <div className="p-4 border border-gray-200 shadow transition-transform ease-in-out duration-300 hover:translate-y-[-5px]">
       <a href={url} target="blank" className="flex gap-4">
         <img src={thumbUrl} alt={title} className="object-cover w-[100px]" />
         <span className="text-left">{title}</span>
