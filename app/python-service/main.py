af0dc9ad8daf3bf00a063a6bccc3ad2ef6db1c72
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-09-22 03:01:38 +0100

    added keyword extraction and paraphrasing models and api endpoints

--- app/python-service/main.py
+++ app/python-service/main.py
@@ -1,7 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from keybert import KeyBERT
+from sentence_transformers import SentenceTransformer
+from transformers import pipeline
 
 app = FastAPI()
 
+model_name = "paraphrase-MiniLM-L6-v2"
+model = SentenceTransformer(model_name)
+kw_model = KeyBERT(model)
+
+paraphraser = pipeline("text2text-generation", model="t5-small")
+
+
 @app.get("/")
 def index():
-  return {"message": "Hello from python"}
+    return {"message": "Hello from python"}
+
+
+@app.get("/get-keywords")
+async def get_keywords(request: Request):
+    body: dict = await request.json()
+    rawText = body.get("raw")
+
+    result = kw_model.extract_keywords(rawText)
+
+    return {"result": result}
+
+
+@app.get("/paraphrase")
+async def paraphrase(request: Request):
+    body: dict = await request.json()
+    rawText = body.get("raw")
+
+    result = ""
+    index = 1
+    paraphrases = paraphraser(rawText, num_return_sequences=1, num_beams=5)
+    for phrase in paraphrases:
+        result += str(index) + " " + phrase.get("generated_text")
+        index += 1
+
+    return {"result": result}
