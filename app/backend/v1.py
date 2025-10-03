f8d500240c62ed231f62f32c63e3f868db7d682c
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-10-03 21:17:50 +0100

    added deptry config file and removed unused deps

--- app/backend/v1.py
+++ app/backend/v1.py
@@ -26,20 +26,4 @@ async def get_keywords(request: Request):
     return {"result": result}
 
 
-@router.get("/paraphrase")
-async def paraphrase(request: Request):
-    body: dict = await request.json()
-    rawText = body.get("raw")
-
-    if rawText is None:
-        return {"error": "Missing 'raw' field in request body."}
-
-    result = ""
-    index = 1
-    paraphrases = paraphraser(rawText, num_return_sequences=1, num_beams=5)
-    for phrase in paraphrases:
-        generated_text = phrase.get("generated_text") or ""
-        result += str(index) + " " + generated_text
-        index += 1
-
-    return {"result": result}
+# @router.get("/extract-content")
