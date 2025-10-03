558bd1074aec7c60d34d43509e6e2d49f10ab3e4
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-10-03 22:06:57 +0100

    added pdf extraction endpoint

--- app/backend/v1.py
+++ app/backend/v1.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, File, UploadFile
 from keybert import KeyBERT
 from sentence_transformers import SentenceTransformer
 from transformers import pipeline
+import pymupdf
 
 
 model_name = "paraphrase-MiniLM-L6-v2"
@@ -26,4 +27,15 @@ async def get_keywords(request: Request):
     return {"result": result}
 
 
-# @router.get("/extract-content")
+@router.get("/extract-pdf-content")
+async def extract_pdf_content(file: UploadFile = File(...)):
+    pdf_bytes = await file.read()
+
+    doc = pymupdf.open(stream=pdf_bytes, filetype="pdf")
+
+    parsed_text = ""
+    for page_num in range(len(doc)):
+        page = doc[page_num]
+        parsed_text += page.get_text("text")  # type: ignore
+
+    return {"content": parsed_text, "filename": file.filename, "pages": len(doc)}
