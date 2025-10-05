aaddeb396e63af6c0d08363564f99e1c9780fc27
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-10-05 02:10:54 +0100

    improved hero text

--- app/frontend/src/components/pages/Landing.tsx
+++ app/frontend/src/components/pages/Landing.tsx
@@ -1,4 +1,3 @@
-import ovalMarker from "@/assets/oval marker.svg";
 import Dropzone from "@/components/Dropzone";
 import Layout from "@/components/Layout";
 export default function Landing() {
@@ -6,14 +5,8 @@ export default function Landing() {
     <Layout>
       <section className="mt-[100px] px-4 max-w-7xl mx-auto text-center">
         <h1>
-          Find keywords that help you find resources for your document{" "}
-          <span className="relative text-amber-300">
-            <img
-              src={ovalMarker}
-              className="absolute transform right-[-10px] top-[-10px] scale-[200%] "
-            />
-            <span>better</span>
-          </span>
+          Find the keywords that lead you to the best resources for your
+          document
         </h1>
         <div className="min-h-[300px] max-w-7xl mt-32 mb-16">
           <Dropzone />
