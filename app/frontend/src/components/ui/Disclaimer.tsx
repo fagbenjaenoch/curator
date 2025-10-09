0303d535d9cfb2a9edaacce34318a0a51f218727
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-10-09 17:42:02 +0100

    added a disclaimer component

---
+++ app/frontend/src/components/ui/Disclaimer.tsx
@@ -0,0 +1,8 @@
+export default function Disclaimer() {
+  return (
+    <div className="border border-amber-500 text-amber-500 bg-amber-100 rounded-md p-4 max-w-[50rem] mx-auto">
+      Disclaimer: This app is very experimental and the result produced should
+      be reviewed.
+    </div>
+  );
+}
