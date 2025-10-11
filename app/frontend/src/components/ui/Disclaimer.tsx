d8fc0b4143a6c7a1267b0e43da5b09f5e7e8fe1a
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-10-11 22:10:53 +0100

    improved disclaimer text

--- app/frontend/src/components/ui/Disclaimer.tsx
+++ app/frontend/src/components/ui/Disclaimer.tsx
@@ -1,7 +1,7 @@
 export default function Disclaimer() {
   return (
     <div className="border border-amber-500 text-amber-500 bg-amber-100 rounded-md p-4 max-w-[50rem] mx-auto">
-      Note: This app is under active development. The results may not be
+      Please note: This app is under active development. The results may not be
       accurate but will improve over time as we refine the system.
     </div>
   );
