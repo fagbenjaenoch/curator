6afe8ea0626f00483c6a54fcb41320fa27876a3e
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-10-14 20:54:03 +0100

    reduced navbar padding on mobile

--- app/frontend/src/components/Navbar.tsx
+++ app/frontend/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Github from "@/components/ui/Github";
 
 export default function Navbar() {
   return (
-    <div className="p-8 bg-amber-300 text-black font-extrabold">
+    <div className="p-4 md:p-8 bg-amber-300 text-black font-extrabold">
       <div className="max-w-7xl mx-auto flex justify-between">
         <span className="text-lg">Curator.</span>
         <div className="flex gap-2">
