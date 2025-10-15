57432b89f3a09bee20ad4b930bd9efdaa8e6b477
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-10-15 07:01:56 +0100

    align navbar elements horizontally

--- app/frontend/src/components/Navbar.tsx
+++ app/frontend/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Github from "@/components/ui/Github";
 export default function Navbar() {
   return (
     <div className="p-4 md:p-8 bg-amber-300 text-black font-extrabold">
-      <div className="max-w-7xl mx-auto flex justify-between">
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
         <span className="md:text-lg">Curator.</span>
         <div className="flex gap-2">
           <Button className="cursor-pointer md:font-extrabold" asChild>
