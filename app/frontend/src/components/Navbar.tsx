73f81d69240a466065183d06b29d6d4dd60bc55a
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-10-15 07:51:10 +0100

    reduced navbar button on mobile

--- app/frontend/src/components/Navbar.tsx
+++ app/frontend/src/components/Navbar.tsx
@@ -6,8 +6,20 @@ export default function Navbar() {
     <div className="p-4 md:p-8 bg-amber-300 text-black lg:font-extrabold">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <span className="md:text-lg">Curator.</span>
-        <div className="flex gap-2">
+        <div className="hidden md:block">
           <Button className="cursor-pointer md:font-extrabold" asChild>
+            <a href="https://github.com/fagbenjaenoch/curator">
+              <Github />
+              Contribute
+            </a>
+          </Button>
+        </div>
+        <div className="md:hidden">
+          <Button
+            size={"sm"}
+            className="cursor-pointer md:font-extrabold text-sm"
+            asChild
+          >
             <a href="https://github.com/fagbenjaenoch/curator">
               <Github />
               Contribute
