bd813f99b24d596a52361870b2e06dd88bfb23d2
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-10-15 07:56:03 +0100

    reduced font weight on mobile

--- app/frontend/src/components/Navbar.tsx
+++ app/frontend/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Github from "@/components/ui/Github";
 
 export default function Navbar() {
   return (
-    <div className="p-4 md:p-8 bg-amber-300 text-black lg:font-extrabold">
+    <div className="p-4 md:p-8 bg-amber-300 text-black font-normal lg:font-extrabold">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <span className="md:text-lg">Curator.</span>
         <div className="hidden md:block">
