a7b4c4d8080934fbcc4ef76954900b3cb01c2a36
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-10-21 06:45:45 +0100

    removed langchain caching in embedding layer

--- app/backend/v1.py
+++ app/backend/v1.py
@@ -1,50 +1,11 @@
 import asyncio
-from typing import List
-
-# import redis
 import pymupdf
 from fastapi import APIRouter, Request, File, UploadFile
 from keybert import KeyBERT
-from keybert.backend import BaseEmbedder
-from langchain_huggingface import HuggingFaceEmbeddings
 
-# from langchain_community.cache import RedisCache, InMemoryCache
-from langchain_community.storage import RedisStore
-from langchain_core.stores import InMemoryByteStore
-from langchain_classic.embeddings.cache import CacheBackedEmbeddings
-import numpy as np
-
-# redis_client = redis.Redis(host="localhost", port=6379, db=0)
-redis_store = RedisStore(redis_url="redis://localhost:6379", namespace="keybert-cache")
-
-in_memory_store = InMemoryByteStore()
 
 model_name = "paraphrase-MiniLM-L6-v2"
-underlying_embeddings = HuggingFaceEmbeddings(model_name=model_name)
-cached_embedder = CacheBackedEmbeddings.from_bytes_store(
-    underlying_embeddings,
-    document_embedding_cache=in_memory_store,
-    namespace=underlying_embeddings.model_name,
-    key_encoder="sha256",
-)
-
-
-class LangChainEmbedder(BaseEmbedder):
-
-    def __init__(
-        self,
-        embedding_model,
-    ):
-        super().__init__()
-        self.embedding_model = embedding_model
-
-    def embed(self, documents: List[str], verbose: bool = False) -> np.ndarray:
-        # TODO: add a check below if documents is not a type of List[str]
-        embeddings = self.embedding_model.embed_documents(documents)
-        return np.array(embeddings)
-
-
-kw_model = KeyBERT(model=LangChainEmbedder(cached_embedder))  # type: ignore
+kw_model = KeyBERT(model_name)  # type: ignore
 
 router = APIRouter()
 
