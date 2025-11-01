397f184eeb8a1b5411d8e8d0b8db5e63f3c65138
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-11-01 16:31:35 +0100

    added horizontal padding on result body on mobile

--- app/frontend/src/components/pages/Landing.tsx
+++ app/frontend/src/components/pages/Landing.tsx
@@ -3,12 +3,12 @@ import Layout from "@/components/Layout";
 export default function Landing() {
   return (
     <Layout>
-      <section className="mt-[100px] px-4 max-w-7xl mx-auto text-center">
+      <section className="mt-[100px] px-8 max-w-7xl mx-auto text-center">
         <h1>
           Find the keywords that lead you to the best resources for your
           document
         </h1>
-        <div className="min-h-[300px] max-w-7xl mt-32 mb-16">
+        <div className="min-h-[300px] mt-32 mb-16">
           <Dropzone />
         </div>
       </section>
