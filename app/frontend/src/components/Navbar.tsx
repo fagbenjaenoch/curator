d6f614261c6c5d76c95e08da1f5ff3f4862fb9c3
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-11-01 16:32:28 +0100

    improved navbar styling on mobile

--- app/frontend/src/components/Navbar.tsx
+++ app/frontend/src/components/Navbar.tsx
@@ -3,23 +3,11 @@ import Github from "@/components/ui/Github";
 
 export default function Navbar() {
   return (
-    <div className="p-4 md:p-8 bg-amber-300 text-black lg:font-extrabold">
+    <div className="p-4 px-8 lg:p-8 bg-amber-300 text-black font-extrabold">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
-        <span className="md:text-lg">Curator.</span>
-        <div className="hidden md:block">
-          <Button className="cursor-pointer md:font-extrabold" asChild>
-            <a href="https://github.com/fagbenjaenoch/curator">
-              <Github />
-              Contribute
-            </a>
-          </Button>
-        </div>
-        <div className="md:hidden">
-          <Button
-            size={"sm"}
-            className="cursor-pointer font-medium lg:font-extrabold text-sm"
-            asChild
-          >
+        <span className="lg:text-lg">Curator.</span>
+        <div>
+          <Button className="cursor-pointer font-extrabold" asChild>
             <a href="https://github.com/fagbenjaenoch/curator">
               <Github />
               Contribute
