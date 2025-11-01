0ef93d780ba6b3e97146aa3c6370b4ae65c2f406
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-11-01 18:54:21 +0100

    left align disclaimer text

--- app/frontend/src/components/ui/Disclaimer.tsx
+++ app/frontend/src/components/ui/Disclaimer.tsx
@@ -2,9 +2,9 @@ import { InfoIcon } from "lucide-react";
 
 export default function Disclaimer() {
   return (
-    <div className="border border-amber-500 text-amber-500 bg-amber-100 rounded-md p-4 max-w-[50rem] mx-auto flex">
+    <div className="border border-amber-500 text-amber-500 bg-amber-100 rounded-md p-4 max-w-[50rem] mx-auto flex gap-4">
       <InfoIcon size={20} className="shrink-0" />
-      <p>
+      <p className="text-left">
         Please note: This app is under active development. The results may not
         be accurate but will improve over time as we refine the system. For best
         experience upload a file less than 100KB.
